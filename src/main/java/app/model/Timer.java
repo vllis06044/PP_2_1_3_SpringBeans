bb54bbb9811a0694be960e98815e758178976ad9
vllis06044 <vl.lisnenko@yandex.ru>
2025-03-18 16:55:28 +0300

    добавлены изменения

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,9 @@
 package app.model;
 
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
